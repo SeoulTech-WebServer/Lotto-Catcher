bcc40da717f3fc1ea19bf954dd5218cbeca3171c
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-29 14:21:10 +0900

    [fix] 디자인 수정

--- src/main/java/seoultech/webserver/lotto/controller/view/dto/LottoViewResponse.java
+++ src/main/java/seoultech/webserver/lotto/controller/view/dto/LottoViewResponse.java
@@ -11,63 +11,80 @@ import seoultech.webserver.lotto.domain.history.History;
 @AllArgsConstructor
 public class LottoViewResponse {
 
-  private Long id;
-  private Integer round;
-  private String lottoryAt;
-  private Integer firstWinnerCount;
-  private Integer secondWinnerCount;
-  private Integer thirdWinnerCount;
-  private Integer fourthWinnerCount;
-  private Integer fifthWinnerCount;
+    private Long id;
+    private Integer round;
+    private String lottoryAt;
+    private Integer firstWinnerCount;
+    private Integer secondWinnerCount;
+    private Integer thirdWinnerCount;
+    private Integer fourthWinnerCount;
+    private Integer fifthWinnerCount;
 
-  private Long firstWinnerAmount;
+    private String firstWinnerAmount;
 
-  private Long secondWinnerAmount;
+    private Long secondWinnerAmount;
 
-  private Long thirdWinnerAmount;
+    private Long thirdWinnerAmount;
 
-  private Long fourthWinnerAmount;
+    private Long fourthWinnerAmount;
 
-  private Long fifthWinnerAmount;
+    private Long fifthWinnerAmount;
 
-  private LottoNumberResponse numberOne;
+    private LottoNumberResponse numberOne;
 
-  private LottoNumberResponse numberTwo;
+    private LottoNumberResponse numberTwo;
 
-  private LottoNumberResponse numberThree;
+    private LottoNumberResponse numberThree;
 
-  private LottoNumberResponse numberFour;
+    private LottoNumberResponse numberFour;
 
-  private LottoNumberResponse numberFive;
+    private LottoNumberResponse numberFive;
 
-  private LottoNumberResponse numberSix;
+    private LottoNumberResponse numberSix;
 
-  private LottoNumberResponse numberBonus;
+    private LottoNumberResponse numberBonus;
 
-  public static LottoViewResponse of(History history) {
-    return LottoViewResponse.builder()
-        .id(history.getId())
-        .round(history.getRound())
-        .lottoryAt(history.getLotteryAt().toString())
+    public static LottoViewResponse of(History history) {
+        return LottoViewResponse.builder()
+                .id(history.getId())
+                .round(history.getRound())
+                .lottoryAt(history.getLotteryAt().toString())
 
-        .firstWinnerCount(history.getFifthWinnerCount())
-        .secondWinnerCount(history.getSecondWinnerCount())
-        .thirdWinnerCount(history.getThirdWinnerCount())
-        .fourthWinnerCount(history.getFourthWinnerCount())
-        .fifthWinnerCount(history.getFirstWinnerCount())
+                .firstWinnerCount(history.getFifthWinnerCount())
+                .secondWinnerCount(history.getSecondWinnerCount())
+                .thirdWinnerCount(history.getThirdWinnerCount())
+                .fourthWinnerCount(history.getFourthWinnerCount())
+                .fifthWinnerCount(history.getFirstWinnerCount())
 
-        .firstWinnerAmount(history.getFirstWinnerAmount())
-        .secondWinnerAmount(history.getSecondWinnerAmount())
-        .thirdWinnerAmount(history.getThirdWinnerAmount())
-        .fourthWinnerAmount(history.getFourthWinnerAmount())
-        .fifthWinnerAmount(history.getFifthWinnerAmount())
-        .numberOne(new LottoNumberResponse(history.getNumberOne()))
-        .numberTwo(new LottoNumberResponse(history.getNumberTwo()))
-        .numberThree(new LottoNumberResponse(history.getNumberThree()))
-        .numberFour(new LottoNumberResponse(history.getNumberFour()))
-        .numberFive(new LottoNumberResponse(history.getNumberFive()))
-        .numberSix(new LottoNumberResponse(history.getNumberSix()))
-        .numberBonus(new LottoNumberResponse(history.getNumberBonus()))
-        .build();
-  }
+                .firstWinnerAmount(toWinAmount(history.getFirstWinnerAmount()))
+                .secondWinnerAmount(history.getSecondWinnerAmount())
+                .thirdWinnerAmount(history.getThirdWinnerAmount())
+                .fourthWinnerAmount(history.getFourthWinnerAmount())
+                .fifthWinnerAmount(history.getFifthWinnerAmount())
+                .numberOne(new LottoNumberResponse(history.getNumberOne()))
+                .numberTwo(new LottoNumberResponse(history.getNumberTwo()))
+                .numberThree(new LottoNumberResponse(history.getNumberThree()))
+                .numberFour(new LottoNumberResponse(history.getNumberFour()))
+                .numberFive(new LottoNumberResponse(history.getNumberFive()))
+                .numberSix(new LottoNumberResponse(history.getNumberSix()))
+                .numberBonus(new LottoNumberResponse(history.getNumberBonus()))
+                .build();
+    }
+
+    private static String toWinAmount(Long amount) {
+        String winAmount = amount.toString();
+        String[] amountStr = winAmount.split("");
+        StringBuilder tmp = new StringBuilder();
+        int cnt = 0;
+        for (int i = amountStr.length - 1; i >= 0; i--) {
+            if (cnt == 3) {
+                tmp.append(",");
+                cnt = 0;
+            }
+            tmp.append(amountStr[i]);
+            cnt++;
+        }
+        winAmount = tmp.reverse().toString();
+        return winAmount;
+    }
 }
